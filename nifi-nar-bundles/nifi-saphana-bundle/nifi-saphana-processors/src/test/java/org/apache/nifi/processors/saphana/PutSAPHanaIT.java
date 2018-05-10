02a8d9811deecd02c7604ca17ba7fee0035a36dd
周国梁 <guoliang.zhou@baishancloud.com>
2018-05-10 20:39:20 +0800

    Merge pull request #13 in DATA/nifi from ORCHSYM-789 to master
    Squashed commit of the following:
    
    commit 12e2e020312dfede6ef3afd51f0c7d63665da3ff
    Author: hjy <jinyong.hu@baishancloud.com>
    Date:   Thu May 10 19:06:41 2018 +0800
    
        ORCHSYM-789 完善PutSAPHana的测试用例,修复字段大小写匹配问题
    
    commit 768f4a20bef190e18fe9883736d1d18414de2569
    Author: hjy <jinyong.hu@baishancloud.com>
    Date:   Thu May 10 16:41:30 2018 +0800
    
        ORCHSYM-789 完善PutSAPHana的测试用例
    
    commit 597475e225de3f701080775c0ac7ed077054afdc
    Author: 周国梁 <guoliang.zhou@baishancloud.com>
    Date:   Thu May 10 15:41:44 2018 +0800
    
        ORCHSYM-789 Clean up format
    
    commit 1e4089631b0957ed9668e823e0b68c87d17a8761
    Author: 周国梁 <guoliang.zhou@baishancloud.com>
    Date:   Thu May 10 15:14:29 2018 +0800
    
        ORCHSYM-789  Add connection pool support
        - Add Put integration tests
    
    commit df1f0a617923064115364cafb8e343cc09a8b795
    Author: 周国梁 <guoliang.zhou@baishancloud.com>
    Date:   Thu May 10 14:57:50 2018 +0800
    
        ORCHSYM-789 SAP HANA数据库支持
        - Copy database utils classes to avoid modify existing processors
    
    commit 2a0968e4e6ca3b80aeb7d7ec8b96c3fcfa5e0e52
    Author: 周国梁 <guoliang.zhou@baishancloud.com>
    Date:   Thu May 10 09:19:20 2018 +0800
    
        ORCHSYM-890 Include package SAP Hana bundle
    
    commit 06d828e6d0fde82686e4b0aedcd2fcedc32c6c6d
    Author: 周国梁 <guoliang.zhou@baishancloud.com>
    Date:   Wed May 9 23:44:08 2018 +0800
    
        ORCHSYM-789 SAP HANA数据库支持
        - Clean branch

---
+++ nifi-nar-bundles/nifi-saphana-bundle/nifi-saphana-processors/src/test/java/org/apache/nifi/processors/saphana/PutSAPHanaIT.java
@@ -0,0 +1,161 @@
+package org.apache.nifi.processors.saphana;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import org.apache.nifi.dbcp.DBCPService;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.reporting.InitializationException;
+import org.apache.nifi.util.MockFlowFile;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.Test;
+
+/***
+ * baishanTable表源数据
+ *     A,B
+ *     1,"a"
+ *     2,"b"
+ *     3,"c"
+ *     4,"d"
+ *     5,"ee"
+ * */
+public class PutSAPHanaIT {
+
+    @Test
+    public void testOnTriggerProcessContextProcessSession() throws InitializationException, FileNotFoundException {
+        updateTable();
+        checkTableRowCount();
+        recoverTable();
+    }
+    
+    /**
+     * 更新数据
+     * */
+    public void updateTable() throws InitializationException, FileNotFoundException {
+        final DBCPService dbcp = new ConnectionFactory();
+        final Map<String, String> dbcpProperties = new HashMap<>();
+        final TestRunner runner = TestRunners.newTestRunner(PutSAPHana.class);
+        runner.addControllerService("dbcp", dbcp, dbcpProperties);
+        runner.enableControllerService(dbcp);
+        runner.setProperty(PutSAPHana.DBCP_SERVICE, "dbcp");
+        runner.setProperty(PutSAPHana.TABLE_NAME, String.valueOf("baishanTable"));
+        runner.setProperty(PutSAPHana.BATCHSIZE, String.valueOf("2"));
+        runner.setProperty(PutSAPHana.KEYCOLUMN, String.valueOf("A"));
+        runner.setIncomingConnection(true);
+        ProcessContext processContext = runner.getProcessContext();
+        PutSAPHana processor = (PutSAPHana) runner.getProcessor();
+        processor.updateScheduledTrue();
+        ProcessSession session = runner.getProcessSessionFactory().createSession();
+        FlowFile flowFile1 = session.create();
+        flowFile1 = session.importFrom(new FileInputStream(new File("C:\\runtime\\saphana\\avro.txt")), flowFile1);
+        runner.enqueue(flowFile1);
+        processor.onTrigger(processContext, runner.getProcessSessionFactory());
+    }
+
+    /**
+     * 检查更新数据是否正确
+     * */
+    public void checkTableRowCount() throws InitializationException {
+        final DBCPService dbcp = new ConnectionFactory();
+        final Map<String, String> dbcpProperties = new HashMap<>();
+        final TestRunner runner = TestRunners.newTestRunner(GetSAPHana.class);
+        runner.addControllerService("dbcp", dbcp, dbcpProperties);
+        runner.enableControllerService(dbcp);
+        runner.setProperty(GetSAPHana.DBCP_SERVICE, "dbcp");
+        runner.setProperty(GetSAPHana.SQL_SELECT_QUERY, String.valueOf("select * from baishanTable"));
+        runner.setIncomingConnection(false);
+        ProcessContext processContext = runner.getProcessContext();
+        GetSAPHana processor = (GetSAPHana) runner.getProcessor();
+        processor.updateScheduledTrue();
+        processor.onTrigger(processContext, runner.getProcessSessionFactory());
+        final List<MockFlowFile> flowFiles = runner.getFlowFilesForRelationship(GetSAPHana.REL_SUCCESS);
+        long totalFlowFilesSize = 0;
+        long rowCount = 0;
+        for (final MockFlowFile flowFile : flowFiles) {
+            rowCount = Long.parseLong(flowFile.getAttribute(GetSAPHana.RESULT_ROW_COUNT));
+            System.out.println(flowFile);
+            totalFlowFilesSize += flowFile.getSize();
+            System.out.println(new String(flowFile.toByteArray()));
+        }
+        System.out.println("rowCount：" + rowCount);
+        Assert.assertEquals(6, rowCount);
+    }
+
+    /**
+     * 还原数据
+     * */
+    public void recoverTable() throws InitializationException {
+        delete6();
+        update5();
+        checkRecovery();
+    }
+    
+    /**
+     * 检查还原数据
+     * */
+    private void checkRecovery() throws InitializationException {
+        final DBCPService dbcp = new ConnectionFactory();
+        final Map<String, String> dbcpProperties = new HashMap<>();
+        final TestRunner runner = TestRunners.newTestRunner(GetSAPHana.class);
+        runner.addControllerService("dbcp", dbcp, dbcpProperties);
+        runner.enableControllerService(dbcp);
+        runner.setProperty(GetSAPHana.DBCP_SERVICE, "dbcp");
+        runner.setProperty(GetSAPHana.SQL_SELECT_QUERY, String.valueOf("select * from baishanTable"));
+        runner.setIncomingConnection(false);
+        ProcessContext processContext = runner.getProcessContext();
+        GetSAPHana processor = (GetSAPHana) runner.getProcessor();
+        processor.updateScheduledTrue();
+        processor.onTrigger(processContext, runner.getProcessSessionFactory());
+        final List<MockFlowFile> flowFiles = runner.getFlowFilesForRelationship(GetSAPHana.REL_SUCCESS);
+        long totalFlowFilesSize = 0;
+        long rowCount = 0;
+        for (final MockFlowFile flowFile : flowFiles) {
+            rowCount = Long.parseLong(flowFile.getAttribute(GetSAPHana.RESULT_ROW_COUNT));
+            System.out.println(flowFile);
+            totalFlowFilesSize += flowFile.getSize();
+            System.out.println(new String(flowFile.toByteArray()));
+        }
+        System.out.println("rowCount：" + rowCount);
+        Assert.assertEquals(5, rowCount);
+
+    }
+
+    private void update5() throws InitializationException {
+        final DBCPService dbcp = new ConnectionFactory();
+        final Map<String, String> dbcpProperties = new HashMap<>();
+        final TestRunner runner = TestRunners.newTestRunner(GetSAPHana.class);
+        runner.addControllerService("dbcp", dbcp, dbcpProperties);
+        runner.enableControllerService(dbcp);
+        runner.setProperty(GetSAPHana.DBCP_SERVICE, "dbcp");
+        runner.setProperty(GetSAPHana.SQL_SELECT_QUERY, String.valueOf("update baishanTable set B='ee' where A=5"));
+        runner.setIncomingConnection(false);
+        ProcessContext processContext = runner.getProcessContext();
+        GetSAPHana processor = (GetSAPHana) runner.getProcessor();
+        processor.updateScheduledTrue();
+        processor.onTrigger(processContext, runner.getProcessSessionFactory());
+    }
+
+    private void delete6() throws InitializationException {
+        final DBCPService dbcp = new ConnectionFactory();
+        final Map<String, String> dbcpProperties = new HashMap<>();
+        final TestRunner runner = TestRunners.newTestRunner(GetSAPHana.class);
+        runner.addControllerService("dbcp", dbcp, dbcpProperties);
+        runner.enableControllerService(dbcp);
+        runner.setProperty(GetSAPHana.DBCP_SERVICE, "dbcp");
+        runner.setProperty(GetSAPHana.SQL_SELECT_QUERY, String.valueOf("delete from baishanTable where A=6"));
+        runner.setIncomingConnection(false);
+        ProcessContext processContext = runner.getProcessContext();
+        GetSAPHana processor = (GetSAPHana) runner.getProcessor();
+        processor.updateScheduledTrue();
+        processor.onTrigger(processContext, runner.getProcessSessionFactory());
+    }
+}
