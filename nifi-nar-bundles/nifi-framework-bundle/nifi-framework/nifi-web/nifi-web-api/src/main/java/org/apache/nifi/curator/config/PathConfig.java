20cfd2c8c14dfd3fe2ffabb62e5133e2b3a4d8af
辜国强 <guoqiang.gu@baishancloud.com>
2019-08-22 16:00:55 +0800

    Merge pull request #238 in DATA/nifi from update_license to master
    Squashed commit of the following:
    
    commit 67ec8502955d4b981738dd2232a724a60129c264
    Author: 刘勋 <xun.liu@baishancloud.com>
    Date:   Thu Aug 22 15:43:44 2019 +0800
    
        更新license

--- nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/main/java/org/apache/nifi/curator/config/PathConfig.java
+++ nifi-nar-bundles/nifi-framework-bundle/nifi-framework/nifi-web/nifi-web-api/src/main/java/org/apache/nifi/curator/config/PathConfig.java
@@ -1,12 +1,13 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Orchsym Runtime under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * this file to You under the Orchsym License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://github.com/orchsym/runtime/blob/master/orchsym/LICENSE
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
